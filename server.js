9c8d97ff7d79553399e9d43b1d372f2099a14008
Ramanan172 <sothinathanramanan@gmail.com>
2024-08-01 11:51:12 +0530

    post request

--- server.js
+++ server.js
@@ -3,14 +3,22 @@ const express = require('express');
 
 //create an instance of express
 const app = express();
+app.use(express.json())
+
+//Sample in-memory storage for item
+let todos = [];
 
 //Create a new todo item
 app.post('/todos',(req,res)=>{
     const {title,description} = req.body;
     const newTodo = {
-        id:0,
+        id:todos.length +1,
         title,
-    }
+        description
+    };
+    todos.push(newTodo);
+    console.log(todos);
+    res.status(201).json(newTodo);
 })
 
 
