024cd3fe94c9eaac10a47ed714ccf3365ede488b
Ramanan172 <sothinathanramanan@gmail.com>
2024-08-01 09:23:49 +0530

    added server code

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const express = require('express');
 
 //create an instance of express
 const app = express();
-//Define a route
-app.get('/',(req,res) =>{
-    res.send("Hello world!")
+
+//Create a new todo item
+app.post('/todos',(req,res)=>{
+    const {title,description} = req.body;
+    const newTodo = {
+        id:0,
+        title,
+    }
 })
+
 
 //start the server
 const port = 3000;
